ab6aea20c2621bc1bb11a6670ea57ee23874d1c3
Damet <damet.2401@gmail.com>
2021-02-06 14:29:49 -0500

    chat-websockets

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+
+app.use(express.static(path.join(__dirname + '/public')))
+
+app.get('/', (req, res) => {
+  res.send('hola como estas?')
+})
+
+const server = app.listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'))
+})
+
+const socketIO = require("socket.io");
+const io = socketIO(server);
+
+io.on('connection', socket => {
+
+  socket.on('message', info => {
+    io.sockets.emit('chat', info)
+  })
+})
---
+++ src/public/js/app.js
@@ -0,0 +1,20 @@
+socket = io()
+const messages = document.getElementById('messages')
+const text = document.getElementById('itext')
+const button = document.querySelector('button')
+const nickname = document.getElementById('nickname')
+
+button.addEventListener('click', event => {
+  let info = {
+    user: nickname.value,
+    message: text.value
+  }
+  socket.emit('message', info)
+})
+
+socket.on('chat', info => {
+  let user = `<div class="u">${info.user ? info.user : 'Anonymous'}</div>`
+  let mss = `<div class="m">${info.message}</div>`
+  messages.innerHTML += `<div class="message">${user} ${mss}</div>`
+  messages.scroll(0, messages.scrollHeight)
+})
