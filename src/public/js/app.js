724d7145366e5f88a538e84ba69f0f360ee42db5
Daniel Mercado <damet.2401@gmail.com>
2021-08-30 19:56:59 -0500

    sounds and bottom scroll

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,12 @@
 socket = io()
 const messages = document.getElementById('messages')
 const text = document.getElementById('itext')
-const button = document.querySelector('button')
+const button = document.querySelector('#send')
+const bdown = document.querySelector('#buttom-down')
 const nickname = document.getElementById('nickname')
+
+const audio = new Audio('sounds/iphone-notificacion.mp3')
+let onPage = true
 
 button.addEventListener('click', event => {
   let info = {
@@ -11,10 +15,39 @@ button.addEventListener('click', event => {
   }
   socket.emit('message', info)
 })
+bdown.addEventListener('click', () => {
+  toBottom()
+  bdown.style.display = 'none'
+})
+
+function playSound(){
+  audio.play()
+}
+
+function toBottom(){
+  messages.scroll(0, messages.scrollHeight)
+}
+
+document.addEventListener('visibilitychange', function(event) {
+  if (!document.hidden) {
+    onPage = true
+  } else {
+    onPage = false
+  }
+})
 
 socket.on('chat', info => {
   let user = `<div class="u">${info.user ? info.user : 'Anonymous'}</div>`
   let mss = `<div class="m">${info.message}</div>`
   messages.innerHTML += `<div class="message">${user} ${mss}</div>`
-  messages.scroll(0, messages.scrollHeight)
+
+  if(!onPage){
+    playSound()
+  }
+
+  if(messages.scrollHeight > messages.clientHeight){
+    if(messages.scrollTop >= 0){
+      bdown.style.display = 'block'
+    }
+  }
 })
